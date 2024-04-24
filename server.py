edf4c9fe0934ad6ff11831d981db8ceb7fdfc4c6
Jay Thorat <j.thorat10@gmail.com>
2024-04-24 13:15:17 +0530

    minor fixes

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import dnslib as jay
 from dnslib.dns import DNSRecord,DNSHeader,DNSQuestion,RR,CNAME,A
+from utils import dns_req_msg_parse
 
 
 
@@ -15,16 +16,14 @@ sock.bind(server)
 print("Listening on " + server_address + ":" + str(server_port))
 
 while True:
-	payload, client_address = sock.recvfrom(511)
+	payload, client_address = sock.recvfrom(512)
 	print("Echoing data back to " + str(client_address))
-	print(payload)
-	d = DNSRecord.parse(payload)
-	print(d)
-	request_id = d.header.id
+	
+	parsed_msg = dns_req_msg_parse(payload)
+	request_id = parsed_msg.header.id
 	print(request_id)
-	d1 = DNSRecord(DNSHeader(qr=1,aa=1,ra=1),q=DNSQuestion("jaythorat.in"),a=RR("jaythorat.in",rdata=A("1.23.34.23"),))
+	d1 = DNSRecord(DNSHeader(qr=1,aa=0,ra=1),q=DNSQuestion("jaythorat.in"),a=RR("jaythorat.in",rdata=A("1.23.34.99"),))
 	d1.header.id=request_id
-	print(str(DNSRecord.parse(d.pack())) == str(d))
 	response = d1.pack()
 	print(type(response))
 	sock.sendto(response, (client_address[0],client_address[1]))
---
+++ utils.py
@@ -0,0 +1,16 @@
+from dnslib import *
+from dnslib.dns import DNSRecord,DNSHeader,DNSQuestion,RR,CNAME,A
+
+
+def dns_req_msg_parse(dns_msg: bytes) -> DNSRecord:
+    """
+    Parse DNS message.
+    """
+    return DNSRecord.parse(dns_msg)
+
+def create_dns_resp_msg():
+    response = DNSRecord()
+    pass
+
+def pack_dns_res_msg():
+    pass

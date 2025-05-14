6463dcc5037feafcd1447703d7979ece0387827a
Jay Thorat <dev.jaythorat@gmail.com>
2025-05-14 16:04:33 +0530

    Added exception handeling in upstream resolver

--- core/upstreamResolver.py
+++ core/upstreamResolver.py
@@ -13,6 +13,13 @@ class UpstreamResolver:
         self.sock.settimeout(self.config.getGoogleUpstreamTimeout())
 
     def sendQuery(self, data):
-        self.sock.sendto(data, self.upstreamDNS)
-        response_data, _ = self.sock.recvfrom(512)
-        return response_data
+        try:
+            self.sock.sendto(data, self.upstreamDNS)
+            response_data, _ = self.sock.recvfrom(512)
+            return response_data
+        except socket.timeout:
+            print("Timeout occurred while waiting for upstream DNS response.")
+            return bytes()
+        except Exception as e:
+            print(f"An error occurred while sending query to upstream DNS: {e}")
+            return bytes()

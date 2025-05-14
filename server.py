fff95bd11d8ae019d5e70fd78f92b4b829db6e81
Jay Thorat <dev.jaythorat@gmail.com>
2025-05-14 16:05:08 +0530

    Added parallel processing for requests

--- server.py
+++ server.py
@@ -1,15 +1,17 @@
-
+from concurrent.futures import ThreadPoolExecutor
 from core.DNSMessageHandler import DNSMessageHandler
 from core.DNSServer import DNSServer
+
+def handle_request(data, addr, server):
+    msgHandler = DNSMessageHandler(data)
+    msgHandler.createDNSRespMsg()
+    response = msgHandler.getDNSRespMsg()
+    server.sendResponse(response, addr)
 
 server = DNSServer()
 server.start()
 
-while True:
-    data,addr = server.getRequest()
-    msgHandler = DNSMessageHandler(data)
-    msgHandler.createDNSRespMsg()
-    response = msgHandler.getDNSRespMsg()
-    
-    server.sendResponse(response, addr)
- 
+with ThreadPoolExecutor(max_workers=10000) as executor:
+    while True:
+        data, addr = server.getRequest()
+        executor.submit(handle_request, data, addr, server)
